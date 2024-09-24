af8029467bad0c612302a44e8d421a02d170bf03
sangrikim <ksr0222@midasit.com>
2024-09-24 17:47:08 +0900

    test

--- canvas/src/main.ts
+++ canvas/src/main.ts
@@ -1,24 +1,17 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const canvas = document.querySelector<HTMLCanvasElement>("myCanvas");
+const ctx1_1 = canvas.getContext("2d");
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+ctx1_1.beginPath(); // 선 그리기 시작
+ctx1_1.moveTo(100, 50); // 시작점으로 이동
+ctx1_1.lineTo(300, 50); // 선의 끝점으로 이동
+ctx1_1.lineTo(300, 200); // 선의 끝점으로 이동
+ctx1_1.lineTo(100, 200); // 선의 끝점으로 이동
+ctx1_1.lineTo(100, 50); // 선의 끝점으로 이동
+ctx1_1.lineWidth = 20; // 선 두께 지정
+ctx1_1.strokeStyle = "#00f"; // 선 색 지정
+ctx1_1.lineCap = "square"; // 선 끝부분 정리(butt, round, square)
+ctx1_1.stroke(); // 선을 그림
+ctx1_1.fillStyle = "red"; // 내부에 채울 색 지정
+ctx1_1.fill();
